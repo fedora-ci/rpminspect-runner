27dad014fd2297209456bfdb93b0e32cc0274934
Michal Srb <michal@redhat.com>
2020-10-13 18:45:21 +0200

    Explicitly provide release id

--- rpminspect_runner.sh
+++ rpminspect_runner.sh
@@ -101,4 +101,4 @@ echo
 echo "======================================== Test Output ========================================"
 
 rpminspect -V
-rpminspect -c ${config} --arches x86_64,noarch,src --tests=${test_name} ${before_build} ${after_build}
+rpminspect -c ${config} --arches x86_64,noarch,src --release=${release_id} --tests=${test_name} ${before_build} ${after_build}

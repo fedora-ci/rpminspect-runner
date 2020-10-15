d1ba5c6299c3dfd558d5cbbb271412076cc9de33
Michal Srb <michal@redhat.com>
2020-10-15 19:28:10 +0200

    Fix test name regexp
    "config" test name was problematic as there are several tests that
    contain the word "configuration" in their description.

--- rpminspect_runner.sh
+++ rpminspect_runner.sh
@@ -94,7 +94,7 @@ echo "Comparing ${after_build} with older ${before_build} found in the \"${updat
 echo
 echo "Test description:"
 
-test_description=$(rpminspect -l -v | awk -v RS= -v ORS='\n\n' /${test_name}/)
+test_description=$(rpminspect -l -v | awk -v RS= -v ORS='\n\n' /${test_name}\n/)
 
 echo "${test_description}"
 echo

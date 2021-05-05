27a40a2d1ce928cd2e56fe8b9e563b562280f913
Michal Srb <michal@redhat.com>
2021-05-05 19:38:56 +0200

    Run all inspections at once and cache results
    This fundamentally changes how we run rpminspect and how we treat its
    results, so the patch is huge...
    
    Previous approach when we were running rpminspect separately for each
    inspection was problematic as rpminspect was unpacking sources on every
    invocation. Which was perfectly fine in 99% of cases, but some builds
    can be 100+ gigs unpackaged... And unpacking is CPU-bound operation, and
    machines in CI sometimes don't have enough... so CI would kill
    rpminspect because it was taking too long.. etc. You get the point.
    
    So here, we are going to run rpminspect only once, and then return cached
    results when asked for specific inspection.
    
    This is also getting super-painful to maintain so it's probably time to
    rewrite the whole script in Python.

---
+++ rpminspect_json2text.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+
+import json
+import argparse
+from pathlib import Path
+
+
+# JSON output uses slightly different names
+# for some of the inspections.
+# FIXME: get rid of this once this is fixed upstream:
+# https://github.com/rpminspect/rpminspect/issues/397
+json2text_mapping = {
+    'empty-payload': 'emptyrpm',
+    'lost-payload': 'lostpayload',
+    'header-metadata': 'metadata',
+    'man-pages': 'manpage',
+    'xml-files': 'xml',
+    'elf-object-properties': 'elf',
+    'desktop-entry-files': 'desktop',
+    'dist-tag': 'disttag',
+    'spec-file-name': 'specname',
+    'java-bytecode': 'javabytecode',
+    'changed-files': 'changedfiles',
+    'moved-files': 'movedfiles',
+    'removed-files': 'removedfiles',
+    'added-files': 'addedfiles',
+    'shell-syntax': 'shellsyntax',
+    'dso-deps': 'dsodeps',
+    'kernel-modules': 'kmod',
+    'architectures': 'arch',
+    'path-migration': 'pathmigration',
+    'bad-functions': 'badfuncs'
+}
+
+
+def process_results(results_json, results_dir):
+    """Process rpminspect JSON and split results into individual text files.
+
+    One inspection result per file. Also store the outcome (pass/fail)
+    in a separate file.
+    """
+    with open(results_json) as f:
+        inspections = json.load(f)
+
+        inspections.pop('rpminspect')
+
+        # Add default "skipped" inspection that will be returned when there are no results
+        inspections['skipped'] = [
+            {
+                'message': 'This inspection did not run.',
+                'result': 'INFO'
+            }
+        ]
+
+        for inspection_json_name, inspection in inspections.items():
+            inspection_name = json2text_mapping.get(inspection_json_name) or inspection_json_name
+
+            result_str = ''
+
+            result_str += f'\n{inspection_name}:\n'
+            result_str += '-' * (len(inspection_name) + 1)
+            result_str += '\n\n'
+
+            outcomes = set()
+            for index, result in enumerate(inspection):
+
+                message = result.get('message', '')
+                if message:
+                    result_str += f"{index+1}) {message}\n\n"
+
+                outcome = result.get('result', '')
+                if outcome:
+                    result_str += f"Result: {outcome}\n"
+                    outcomes.add(outcome)
+
+                waiver_auth = result.get('waiver authorization', '')
+                if waiver_auth:
+                    result_str += f"Waiver Authorization: {waiver_auth}\n"
+
+                details = result.get('details', '')
+                if details:
+                    result_str += f"\nDetails:\n{details}\n"
+
+                remedy = result.get('remedy', '')
+                if remedy:
+                    result_str += f"\nSuggested Remedy:\n{remedy}\n"
+
+                result_str += '\n\n'
+
+            # remove all good outcomes
+            outcomes.discard('OK')
+            outcomes.discard('INFO')
+            outcomes.discard('WAIVED')
+
+            status = 0  # success
+            # the outcomes set should be empty now,
+            # if all went well
+            if outcomes:
+                # nope, there are some failures or unknown outcomes...
+                # let's fail the test
+                status = 1  # failure
+
+            result_path = Path(results_dir) / Path(inspection_name + '_result')
+            with open(result_path, 'w') as output_f:
+                output_f.write(result_str)
+
+            status_path = Path(results_dir) / Path(inspection_name + '_status')
+            with open(status_path, 'w') as output_f:
+                output_f.write(str(status))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('resultsdir', help='directory where to store results')
+    parser.add_argument('rpminspectjson', help='JSON output from rpminspect')
+    args = parser.parse_args()
+    process_results(args.rpminspectjson, args.resultsdir)

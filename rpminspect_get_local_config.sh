d7a0b351b8222f1de52a53a931e92292aa032330
Michal Srb <michal@redhat.com>
2022-03-21 17:37:40 +0100

    Fallback to the default config branch, if the main one doesn't exist

--- rpminspect_get_local_config.sh
+++ rpminspect_get_local_config.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Usage:
-# ./rpminspect_get_local_config.sh $AFTER_BUILD
+# ./rpminspect_get_local_config.sh <after-build>
 #
 # The script recognizes following environment variables:
 # KOJI_BIN - path where to find "koji" binary
 # CONFIG_BRANCH - git branch where to look for the rpminspect.yaml
+# DEFAULT_CONFIG_BRANCH - same as CONFIG_BRANCH;
+#     this branch will be used when CONFIG_BRANCH branch doesn't exist or if the CONFIG_BRANCH variable is not set
 
 
 config_branch="${CONFIG_BRANCH}"
+default_config_branch="${DEFAULT_CONFIG_BRANCH}"
 koji_bin="${KOJI_BIN:-/usr/bin/koji}"
 
 after_build="${1}"
@@ -17,21 +20,49 @@ repo_ref=$(${koji_bin} buildinfo ${after_build} | grep "^Source: " | awk '{ prin
 repo_url=$(echo ${repo_ref} | awk -F'#' '{ print $1 }')
 commit_ref=$(echo ${repo_ref} | awk -F'#' '{ print $2 }')
 
+
+find_config_branch() {
+    # Determine in which branch we should look for the rpminspect.yaml file
+    # Params:
+    # $1: config branch
+    # $2: default config brach
+    local release_branch="$1"
+    local default_branch="$2"
+
+    for b in $release_branch $default_branch; do
+        if [ -n "${b}" ]; then
+            # check if branch exists in the remote repository
+            git ls-remote --exit-code --heads "${repo_url}" "refs/heads/${b}" > /dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                echo "${b}"
+                return
+            fi
+        fi
+    done
+
+    echo -n ""
+    return
+}
+
 # obtain the package-specific config file
 if [ ! -f "rpminspect.yaml" ]; then
     (
+        set -x
+        branch=$(find_config_branch "${config_branch}" "${default_config_branch}")
+
         tmp_dir=$(mktemp -d -t rpminspect-XXXXXXXXXX)
 
         pushd ${tmp_dir}
             git init
             git remote add origin ${repo_url}
 
-            if [ -n "${config_branch}" ]; then
+            if [ -n "${branch}" ]; then
                 # we take the config from the HEAD of the given branch
-                git fetch origin "refs/heads/${config_branch}" --depth 1
-                git checkout ${config_branch}
+                git fetch origin "refs/heads/${branch}" --depth 1
+                git checkout ${branch}
             else
                 # we take the config from the build commit
+                echo "Config branch doesn't exist, using the commit hash..."
                 git fetch origin
                 git checkout ${commit_ref}
             fi

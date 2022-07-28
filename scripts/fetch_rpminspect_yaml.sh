398f2586028c3a9fcd839e5d9ac2a79902798e85
Michal Srb <michal@redhat.com>
2022-07-28 07:39:32 +0200

    Add script for fetching local rpminspect yaml file

---
+++ scripts/fetch_rpminspect_yaml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+workdir=${RPMINSPECT_WORKDIR:-${PWD}}
+cache_dir="${workdir}/cache"
+
+component_name=$(cat "${cache_dir}/component_name")
+
+set -x
+
+git clone --depth 1 --branch "${RPMINSPECT_YAML_BRANCH_NAME}" "${DIST_GIT_RPMS_URL}/${component_name}.git" repo
+if [ -f "repo/rpminspect.yaml" ]; then
+    cp "repo/rpminspect.yaml" "${workdir}/rpminspect.yaml"
+fi
+rm -Rf repo/

97ae81cd9585ec10e745b8330e7fc7a0fd4cf266
Martin Pitt <martin@piware.de>
2022-11-08 15:24:08 +0100

    Drop invocation with inspection name and caching
    We are going to call rpminspect_runner.sh just once for a package
    update. All the extra result parsing and splitting is obsolete, it will
    be replaced with making rpminspect's new builtin viewer [1] easily
    accessible from TF results.
    
    [1] https://github.com/rpminspect/rpminspect/tree/main/contrib

--- rpminspect_runner.sh
+++ rpminspect_runner.sh
@@ -39,7 +39,12 @@ koji_bin=${KOJI_BIN:-/usr/bin/koji}
 
 task_id=${1}
 previous_tag=${2}
-test_name=${3}
+
+# This used to be called with a test name; avoid calling with old fmf from rpminspect-pipeline's
+if [ -n "${3:-}" ]; then
+    echo "calling with test name is obsolete" >&2
+    exit 3
+fi
 
 # In case there is no dist tag (like ".fc34") in the package name,
 # rpminspect doesn't know which test configuration to use
@@ -74,24 +79,6 @@ get_ns_from_module_nvr() {
     local nvr=$1
     name=$(echo $nvr | sed 's/^\(.*[^-]\{1,\}\)-\([^-]\{1,\}\)$/\1/')
     echo -n ${name}
-}
-
-quit_if_disabled() {
-    # Quit the script if the inspection is disabled in config/profile file.
-    # Params:
-    # $1: inspection name
-    local inspection_name=$1
-
-    is_enabled=$(python3 -c "\
-import yaml; \
-import sys; \
-is_enabled = yaml.safe_load(open(sys.argv[1])).get('inspections', {}).get(sys.argv[2], True); \
-print('yes', end='') if is_enabled else print('no', end='')" "effective_rpminspect.yaml" "${inspection_name}")
-    if [ "${is_enabled}" == "no" ]; then
-        echo
-        echo "This inspection is disabled."
-        exit 0
-    fi
 }
 
 get_after_build() {
@@ -164,98 +151,82 @@ get_before_module_build() {
     echo -n ${before_build}
 }
 
-# This is where we will cache rpminspect results, so we can later just return them if they already exist
-results_cache_dir="${PWD}/cache_dir"
-results_cached_file="${results_cache_dir}cached"
-
-mkdir -p "${results_cache_dir}"
-
 after_build_param="${task_id}"
 
 before_build=''
 
-# cache results — the following section should run in CI only once
-if [ ! -f "${results_cached_file}" ]; then
-    if [ "${is_module}" == "yes" ]; then
+if [ "${is_module}" == "yes" ]; then
 
-        module_info=$(curl "${MBS_API_URL}/module-build-service/1/module-builds/${task_id}")
-        name=$(echo "${module_info}" | jq -r .name)
-        stream=$(echo "${module_info}" | jq -r .stream)
-        version=$(echo "${module_info}" | jq -r .version)
-        context=$(echo "${module_info}" | jq -r .context)
-        after_build="${name}-${stream}-${version}.${context}"
+    module_info=$(curl "${MBS_API_URL}/module-build-service/1/module-builds/${task_id}")
+    name=$(echo "${module_info}" | jq -r .name)
+    stream=$(echo "${module_info}" | jq -r .stream)
+    version=$(echo "${module_info}" | jq -r .version)
+    context=$(echo "${module_info}" | jq -r .context)
+    after_build="${name}-${stream}-${version}.${context}"
 
-        if [ -n "$previous_tag" ]; then
-            before_build=$(get_before_module_build "${after_build}" "${previous_tag}")
-        fi
-        after_build_param="${after_build}"
-    else
-        after_build=$(get_after_build "${task_id}")
-        if [ -n "$previous_tag" ]; then
-            before_build=$(get_before_build "${after_build}" "${previous_tag}")
-        fi
+    if [ -n "$previous_tag" ]; then
+        before_build=$(get_before_module_build "${after_build}" "${previous_tag}")
     fi
-
-    echo -n "${after_build}" > "${results_cache_dir}/after_build"
-    echo -n "${before_build}" > "${results_cache_dir}/before_build"
-
-    if [ ! -f "effective_rpminspect.yaml" ]; then
-        # Get the effective config file
-        /usr/bin/rpminspect -c ${config} \
-                ${profile_name:+--profile=$profile_name} \
-                -D > effective_rpminspect.yaml || :
+    after_build_param="${after_build}"
+else
+    after_build=$(get_after_build "${task_id}")
+    if [ -n "$previous_tag" ]; then
+        before_build=$(get_before_build "${after_build}" "${previous_tag}")
     fi
+fi
 
-    rpminspect_get_local_config.sh "${after_build}"
+echo -n "${after_build}" > "${results_cache_dir}/after_build"
+echo -n "${before_build}" > "${results_cache_dir}/before_build"
 
-    # Update the virus dababase
-    freshclam > freshclam.log 2>&1 || :
-
-    # Update annobin
-    # FIXME: we don't want to touch packages when the base image is Rawhide...
-    #     We can uncomment this once the latest annocheck can be installed from a stable repo.
-    #dnf update -y annobin* > update_annobin.log 2>&1 || :
-
-    # Update the data package, but from COPR, not from the official Fedora repositories
-    dnf update --disablerepo="fedora*" -y ${RPMINSPECT_PACKAGE_NAME} ${RPMINSPECT_DATA_PACKAGE_NAME} > update_rpminspect.log 2>&1 || :
-
-    output_filename=${TMT_TEST_DATA:-.}/result.json
-    verbose_log=${TMT_TEST_DATA:-.}/verbose.log
-    # Workdir used by rpminspect
-    workdir="${PWD}/workdir/"
-    mkdir -p "${workdir}"
-    # Run all inspections and cache results
+if [ ! -f "effective_rpminspect.yaml" ]; then
+    # Get the effective config file
     /usr/bin/rpminspect -c ${config} \
-            --workdir "${workdir}" \
-            --format=json \
-            --output="${output_filename}" \
-            --verbose \
-            ${arches:+--arches=$arches} \
-            ${default_release_string:+--release=$default_release_string} \
             ${profile_name:+--profile=$profile_name} \
-            ${tests:+--tests=$tests} \
-            ${before_build} \
-            ${after_build_param} \
-            > $verbose_log 2>&1 || :
+            -D > effective_rpminspect.yaml || :
+fi
 
-    rm -Rf "${workdir}"
+rpminspect_get_local_config.sh "${after_build}"
 
-    if [ -f "${output_filename}" ]; then
-        # Convert JSON to text and store results of each inspection to a separate file
-        rpminspect_json2text.py "${results_cache_dir}" "${output_filename}"
-        touch "${results_cached_file}"
-    else
-        # rpminspect probably crashed... let's just show the verbose.log
-        cat $verbose_log
-        exit 123  # error
-    fi
+# Update the virus dababase
+freshclam > freshclam.log 2>&1 || :
+
+# Update annobin
+# FIXME: we don't want to touch packages when the base image is Rawhide...
+#     We can uncomment this once the latest annocheck can be installed from a stable repo.
+#dnf update -y annobin* > update_annobin.log 2>&1 || :
+
+# Update the data package, but from COPR, not from the official Fedora repositories
+dnf update --disablerepo="fedora*" -y ${RPMINSPECT_PACKAGE_NAME} ${RPMINSPECT_DATA_PACKAGE_NAME} > update_rpminspect.log 2>&1 || :
+
+output_filename=${TMT_TEST_DATA:-.}/result.json
+verbose_log=${TMT_TEST_DATA:-.}/verbose.log
+# Workdir used by rpminspect
+workdir="${PWD}/workdir/"
+mkdir -p "${workdir}"
+# Run all inspections
+rc=0
+/usr/bin/rpminspect -c ${config} \
+        --workdir "${workdir}" \
+        --format=json \
+        --output="${output_filename}" \
+        --verbose \
+        ${arches:+--arches=$arches} \
+        ${default_release_string:+--release=$default_release_string} \
+        ${profile_name:+--profile=$profile_name} \
+        ${tests:+--tests=$tests} \
+        ${before_build} \
+        ${after_build_param} \
+        > $verbose_log 2>&1 || rc=$?
+
+rm -Rf "${workdir}"
+
+if [ $rc -gt 1 ]; then
+    # rpminspect probably crashed... let's show the verbose.log
+    cat $verbose_log
 fi
 
 after_build=$(cat "${results_cache_dir}/after_build")
 before_build=$(cat "${results_cache_dir}/before_build")
-
-# Get description for current inspection
-/usr/bin/rpminspect -l -v | awk -v RS= -v ORS='\n\n' "/    ${test_name}\n/" | sed -e 's/^[ \t]*//' | tail -n +2 > "${results_cache_dir}/${test_name}_description"
 
 rpminspect_version=`rpm -q --qf "%{VERSION}-%{RELEASE}" ${RPMINSPECT_PACKAGE_NAME}`
 data_version=`rpm -q --qf "%{VERSION}-%{RELEASE}" ${RPMINSPECT_DATA_PACKAGE_NAME}`
@@ -271,19 +242,5 @@ else
     echo "old build: ${before_build} (found in ${previous_tag} $(basename ${koji_bin}) tag)"
 fi
 echo
-echo "Test description:"
-cat "${results_cache_dir}/${test_name}_description"
-echo "======================================== Test Output ========================================"
-
-quit_if_disabled "${test_name}"
-
-if [ -f "${results_cache_dir}/${test_name}_result" ]; then
-    cat "${results_cache_dir}/${test_name}_result"
-    rc=$(cat "${results_cache_dir}/${test_name}_status")
-else
-    # This inspection did not run (modularity inspection?)
-    cat "${results_cache_dir}/skipped_result"
-    rc=0  # success
-fi
-
-exit $((rc))
+echo "rpminspect exited with status code $rc"
+exit $rc

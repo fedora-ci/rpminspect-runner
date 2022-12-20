fedora-ci/rpminspect-runner

commit fc8f7378b736f51bc456395d333a9c01ca3bc2ec

Expose custom package configuration as artifact
Some packages use a custom `rpminspect.yaml` in their dist-git. If it
exists, attach it as an additional artifact.

Drop the empty default rpminspect.yaml, as we don't want to attach that.
rpminspect does not require this.

Fixes OSCI-3997

rpminspect_get_local_config.sh
@@ -69,8 +69,9 @@ if [ ! -f "rpminspect.yaml" ]; then
         popd
 
         # and finally, copy the config to the current directory;
-        # or create an empty one if missing in the repository
-        cp "${tmp_dir}/rpminspect.yaml" . || echo "inspections: {}" > rpminspect.yaml
+        if [ -f "${tmp_dir}/rpminspect.yaml" ]; then
+            cp cp "${tmp_dir}/rpminspect.yaml" .
+        fi
         rm -Rf "${tmp_dir}"
     ) >> clone.log 2>&1
 fi

rpminspect_runner.sh
@@ -251,6 +251,11 @@ if [ -n "$TMT_TEST_DATA" ]; then
     - data/rpminspect/data/verbose.log
     - data/rpminspect/data/result.json
 EOF
+    # if dist-git uses a custom rpminspect config, add that as an artifact as well
+    if [ -e rpminspect.yaml ]; then
+        cp rpminspect.yaml "$TMT_TEST_DATA"
+        echo "    - data/rpminspect/data/rpminspect.yaml" >> "$TMT_TEST_DATA/results.yaml"
+    fi
     echo "running in TMT, wrote $TMT_TEST_DATA/results.yaml"
 fi
 

commit e280442382f4b993bca590ebfda20db2e066fbce

Fix `cp` typo in rpminspect_get_local_config.sh
Introduced in commit fc8f7378b7. That's what you get with scripts which
aren't `set -e`..

rpminspect_get_local_config.sh
@@ -70,7 +70,7 @@ if [ ! -f "rpminspect.yaml" ]; then
 
         # and finally, copy the config to the current directory;
         if [ -f "${tmp_dir}/rpminspect.yaml" ]; then
-            cp cp "${tmp_dir}/rpminspect.yaml" .
+            cp "${tmp_dir}/rpminspect.yaml" .
         fi
         rm -Rf "${tmp_dir}"
     ) >> clone.log 2>&1
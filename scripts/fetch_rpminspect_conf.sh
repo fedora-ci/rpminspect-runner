14ded15a54ae03e919caca0c14588d3e8343f39a
David Cantrell <dcantrell@redhat.com>
2023-03-07 14:02:37 -0500

    Rename RPMINSPECT_YAML_BRANCH_NAME to RPMINSPECT_CONF_BRANCH_NAME
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- scripts/fetch_rpminspect_conf.sh
+++ scripts/fetch_rpminspect_conf.sh
@@ -10,7 +10,7 @@ component_name=$(cat "${cache_dir}/component_name")
 
 set -x
 
-git clone --depth 1 --branch "${RPMINSPECT_YAML_BRANCH_NAME}" "${DIST_GIT_RPMS_URL}/${component_name}.git" repo
+git clone --depth 1 --branch "${RPMINSPECT_CONF_BRANCH_NAME}" "${DIST_GIT_RPMS_URL}/${component_name}.git" repo
 for ext in ${exts} ; do
     cfgfile="rpminspect.${ext}"
     if [ -f "repo/${cfgfile}" ]; then

5912b194e1736c0f4d3f816929e647bd760af3da
Cristian Le <git@lecris.dev>
2026-01-05 17:56:11 +0100

    Use the new `filter_distro` to better support other distros

--- read_distro.py
+++ read_distro.py
@@ -2,6 +2,7 @@
 # /// script
 # dependencies = [
 #   "fedora-distro-aliases",
+#   "koji",
 #   "click",
 # ]
 # ///
@@ -9,12 +10,27 @@
 import re
 
 import click
-from fedora_distro_aliases import get_distro_aliases
+from fedora_distro_aliases import (
+    filter_distro,
+    Distro,
+    bodhi_active_releases,
+    get_distro_aliases,
+)
+import koji
 
-# TODO: There is no clean way to break down the `fedora-rawhide` from the aliases right now, using
-#   fedora-all alias instead
-# https://github.com/rpm-software-management/fedora-distro-aliases/issues/29
-fedora_all = get_distro_aliases()["fedora-all"]
+aliases = get_distro_aliases()
+# Workaround the fact that eln is not in the aliases right now
+# https://github.com/rpm-software-management/fedora-distro-aliases/issues/30
+eln_distro = [
+    Distro.from_bodhi_release(release)
+    for release in bodhi_active_releases()
+    if release["branch"] == "eln"
+]
+aliases["eln"] = eln_distro
+
+def get_build_macros(build_tag: str):
+    session = koji.ClientSession("https://koji.fedoraproject.org/kojihub")
+    return session.getBuildConfig(build_tag)
 
 
 @click.command()
@@ -23,22 +39,36 @@ def main(distro_name: str) -> None:
     """
     Get the equivalent previous tag from the distro context
     """
-    if fedora_match := re.match(r"fedora-(?P<branch>.+)", distro_name):
-        branch = fedora_match.group("branch")
-        if branch != "rawhide":
-            # For numbered branches (e.g. fedora-42)
-            branch = f"f{branch}"
-    else:
-        # TODO: Deal with epel and eln
-        raise NotImplementedError
-    try:
-        distro_info = next(d for d in fedora_all if d.branch == branch)
-    except StopIteration:
-        click.echo(f"Branch not valid: {branch}", err=True)
+    distro_info = filter_distro(aliases, namever=distro_name)
+    if not distro_info:
+        click.echo(f"Could not identify distro '{distro_name}'", err=True)
         exit(1)
-    # TODO: would be cleaner if we made rpminspect_runner.sh a python script instead
-    print(f"fc{distro_info.version_number}")
-    print(f"f{distro_info.version_number}-updates")
+    # There does not seem to be an easy way to get %{?distro} so we build it manually for each case
+    match distro_info.product:
+        case "fedora":
+            if distro_info.branch == "eln":
+                # Special handling for eln since the `%{?distro}` has is not predictable
+                try:
+                    configs = get_build_macros("eln-build")
+                    eln_macro = configs["extra"]["rpm.macro.eln"]
+                except Exception:
+                    click.echo("Failed to get eln distro tag", err=True)
+                    raise
+                print(f"eln{eln_macro}")
+                print("eln")
+            else:
+                print(f"fc{distro_info.version}")
+                if distro_info.branch == "rawhide":
+                    print(f"f{distro_info.version}")
+                else:
+                    print(f"f{distro_info.version}-updates")
+        case "epel":
+            epel_version: str = distro_info.version
+            print(f"el{epel_version.replace('.','_')}")
+            print(f"epel{epel_version}")
+        case _:
+            click.echo(f"Unrecognized distro.product '{distro_info.product}' of '{distro_name}'", err=True)
+            exit(1)
 
 
 if __name__ == "__main__":
